aa246f95fdcc52dcf8a045040ba298420723ee47
AtikinXX <atikinxx@gmail.com>
2020-06-12 20:47:19 +0300

    fix leeson04

--- script.js
+++ script.js
@@ -7,50 +7,50 @@ let money = prompt('Ваш месячный доход?', '50000'),
   mission = 100000,
   period = 10,
   expenses1 = prompt('Введите обязательную статью расходов?'),
-  amount1 = prompt('Во сколько это обойдется?', '4000'),
+  amount1 = +prompt('Во сколько это обойдется?', '4000'),
   expenses2 = prompt('Введите обязательную статью расходов?'),
-  amount2 = prompt('Во сколько это обойдется?', '11000');
-  
-  
+  amount2 = +prompt('Во сколько это обойдется?', '11000'),
+  accumulatedMonth,
+  budgetDay,
+  missionDone;
 
-function getExpensesMonth(expenses1, expenses2) {
-  let sum = Number(expenses1) + Number(expenses2);
-  return sum;
+function getExpensesMonth(val1, val2) {
+  return val1 + val2;
 }
 getExpensesMonth(amount1, amount2);
-function getAccumulatedMonth(money, expenses1, expenses2) {
-  let accumulatedTotal = Number(money) - getExpensesMonth(expenses1, expenses2);
-  return accumulatedTotal;
+
+function getAccumulatedMonth(val1, val2, val3) {
+  return val1 - getExpensesMonth(val2, val3);
 }
 
 function getTargetMonth(mission, accumulatedMonth) {
-  let targetMonth = Number(mission) / Number(accumulatedMonth);
-  return targetMonth;
+  return mission / accumulatedMonth;
 }
-
-let accumulatedMonth = getAccumulatedMonth(money, amount1, amount2),
-  budgetDay = Math.floor(accumulatedMonth / 30),
-  missionDone = Math.ceil(mission / accumulatedMonth);
-
 
 let showTypeOf = function (data) {
     console.log(data, typeof(data));
   };
-showTypeOf(getExpensesMonth(amount1, amount2));
+
+
+let getStatusIncome = function () {
+  if (budgetDay >= 1200) {
+    return 'У вас высокий уровень дохода';
+  } else if (budgetDay >= 600) {
+    return 'У вас средний уровень дохода';
+  } else if (budgetDay <= 0) {
+    return 'Что то пошло не так';
+  } else {
+    return 'К сожалению у вас уровень дохода ниже среднего';
+  }
+};
+
+accumulatedMonth = +getAccumulatedMonth(money, amount1, amount2);
+budgetDay = Math.floor(accumulatedMonth / 30);
+missionDone = Math.ceil(mission / accumulatedMonth);
+
 showTypeOf(addExpenses.toLowerCase().split(', '));
 showTypeOf(getTargetMonth(mission, accumulatedMonth));
 showTypeOf(budgetDay);
-
-let getStatusIncome = function name(params) {
-  if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-  } else if (budgetDay >= 600) {
-    console.log('У вас средний уровень дохода');
-  } else if (budgetDay <= 0) {
-    console.log('Что то пошло не так');
-  } else {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-  }
-};
-getStatusIncome();
+console.log(getExpensesMonth(amount1, amount2));
+console.log(getStatusIncome());
 
